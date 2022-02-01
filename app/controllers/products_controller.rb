dfb030ecce18e53f825a68dda674e735acce6b80
Sufyan Iqbal <Sufyan.Iqbal@activekeysolutions.com>
2022-02-01 17:23:16 +0500

    here is all on turo update and destroy , time_prof

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-        redirect_to products_path
+      render :create, status: :created
     else
       render :form_update, status: :unprocessable_entity
     end
@@ -32,24 +32,20 @@ class ProductsController < ApplicationController
 
   # PATCH/PUT /products/1 or /products/1.json
   def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
+    if @product.update(product_params)
+      render :create, status: :created
+    else
+        render :form_update, status: :unprocessable_entity
     end
   end
 
   # DELETE /products/1 or /products/1.json
   def destroy
     @product.destroy
-
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
-      format.json { head :no_content }
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(@product)
+      end
     end
   end
 
